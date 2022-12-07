a8e7a3c93f75ba2654baca700ccb61ffafff5f10
Valentin Vladimirov <valentinvladimirov@abv.bg>
2022-12-07 16:29:39 +0200

    task-94

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import "./scss/app.scss";
 import Application from "./js/Application";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
-  const app = new Application();
-  // Used to access the app instance by the automated tests
-  app.setEmojis(["🐒", "🦍", "🦧"]);
-  app.addBananas();
-  window.__JS_APP = app;
+    // This block will be executed once the page is loaded and ready
+    const app = new Application();
+    const p = document.createElement("p");
+    const div = document.getElementById("emojis");
+
+    // Used to access the app instance by the automated tests
+    app.setEmojis(["🐒", "🦍", "🦧"]);
+    let newEmojis = app.addBananas();
+
+    p.innerHTML += newEmojis.map((emoji) => {
+        return emoji;
+    });
+
+    div.appendChild(p);
+    window.__JS_APP = app;
 });
